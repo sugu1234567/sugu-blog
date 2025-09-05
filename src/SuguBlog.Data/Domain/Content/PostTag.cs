203d644709ae4f6288f4c277b7fd39ec2c4fc091
Nam <ngovannam10022003@gmail.com>
2025-09-05 10:06:04 +0700

    tao entities cho content

---
+++ src/SuguBlog.Data/Domain/Content/PostTag.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuguBlog.Data.Domain.Content
+{
+    [Table("PostTags")]
+    [PrimaryKey(nameof(PostId), nameof(TagId))]
+    public class PostTag
+    {
+        public Guid PostId { set; get; }
+        public Guid TagId { set; get; }
+    }
+}
---
+++ src/SuguBlog.Data/Domain/Content/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuguBlog.Data.Domain.Content
+{
+    [Table("Tags")]
+    public class Tag
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public required string Name { get; set; }
+    }
+}
