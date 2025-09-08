c3c0df332314187bbb32c79d888cddd8eff62a1d
Nam <ngovannam10022003@gmail.com>
2025-09-08 15:18:43 +0700

    migration database

---
+++ src/SuguBlog.Data/SuguBlogContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SuguBlog.Data
+{
+    public class SuguBlogContextFactory : IDesignTimeDbContextFactory<SuguBlogContext>
+    {
+        public SuguBlogContext CreateDbContext(string[] args)
+        {
+            // Load config từ appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())      // lấy folder hiện tại
+                .AddJsonFile("appsettings.json")                   // đọc file appsettings.json
+                .Build();
+            // Tạo DbContextOptionsBuilder
+            var builder = new DbContextOptionsBuilder<SuguBlogContext>();
+
+            // Cấu hình dùng SQL Server với connection string "DefaultConnection"
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            // Trả về instance của DbContext
+            return new SuguBlogContext(builder.Options);
+        }
+    }
+}
